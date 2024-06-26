6be94fb936cf3850a342592d8f0ed6db962edb1f
David-McCaig <davidmccaig1@gmail.com>
2024-06-26 15:37:06 +1200

    Create sphere

--- src/app/components/visualizer.js
+++ src/app/components/visualizer.js
@@ -4,45 +4,48 @@ import * as THREE from 'three';
 import { Suspense, useEffect, useRef } from 'react';
 import { Canvas, useFrame } from '@react-three/fiber';
 import { suspend } from 'suspend-react';
+import { AccumulativeShadows, RandomizedLight, Center, Environment, OrbitControls } from '@react-three/drei'
 
 export default function Home(props) {
   return (
-    <Canvas shadows dpr={[1, 2]} camera={{ position: [-1, 1.5, 2], fov: 25 }}>
-      <spotLight position={[-4, 4, -4]} angle={0.06} penumbra={1} castShadow shadow-mapSize={[2048, 2048]} />
+    <Canvas shadows dpr={[1, 2]} camera={{ position: [-1, 1, 2], fov: 10 }}>
+      <spotLight position={[-4, 4, -4]} angle={0.04} penumbra={1} castShadow shadow-mapSize={[2048, 2048]} />
       <Suspense fallback={null}>
         <Track />
       </Suspense>
-      <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.025, 0]}>
+      {/* <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]} position={[0, -0.025, 0]}>
         <planeGeometry />
         <shadowMaterial transparent opacity={0.15} />
-      </mesh>
+      </mesh> */}
     </Canvas>
   );
 }
 
-function Track({ y = 2500, space = 1.8, width = 0.01, height = 0.05, obj = new THREE.Object3D(), ...props }) {
+function Track({ y = 2500, space = 1.8, radius = 0.1, obj = new THREE.Object3D(), ...props }) {
   const ref = useRef();
-  const { gain, context, update, data } = suspend(() => createAudio(), []);
+  const { context, update, data } = suspend(() => createAudio(), []);
+  
+  // Variables for decay and smoothing
+  let smoothAvg = 0;
+  const decayFactor = 0.97;
 
-//   useEffect(() => {
-//     gain.connect(context.destination);
-//     return () => gain.disconnect();
-//   }, [gain, context]);
-
-  useFrame((state) => {
+  useFrame(() => {
     let avg = update();
-    for (let i = 0; i < data.length; i++) {
-      obj.position.set(i * width * space - (data.length * width * space) / 2, data[i] / y, 0);
-      obj.updateMatrix();
-      ref.current.setMatrixAt(i, obj.matrix);
-    }
-    ref.current.material.color.setHSL(avg / 360, 0.75, 0.75);
+
+    // Apply exponential smoothing
+    smoothAvg = avg * (1 - decayFactor) + smoothAvg * decayFactor;
+
+    obj.position.set(0, smoothAvg / y, 0);
+    obj.updateMatrix();
+    ref.current.setMatrixAt(0, obj.matrix);
+    ref.current.material.color.setHSL(smoothAvg / 360, 0.75, 0.75);
     ref.current.instanceMatrix.needsUpdate = true;
+    console.log(ref)
   });
 
   return (
-    <instancedMesh castShadow ref={ref} args={[null, null, data.length]} {...props}>
-      <planeGeometry args={[width, height]} />
+    <instancedMesh castShadow ref={ref} args={[null, null, 1]} {...props}>
+      <sphereGeometry args={[radius, 32, 32]} />
       <meshBasicMaterial toneMapped={false} />
     </instancedMesh>
   );
@@ -52,16 +55,14 @@ async function createAudio() {
   const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
   const context = new (window.AudioContext || window.webkitAudioContext)();
   const source = context.createMediaStreamSource(stream);
-
   const analyser = context.createAnalyser();
   analyser.fftSize = 64;
   source.connect(analyser);
-
   const data = new Uint8Array(analyser.frequencyBinCount);
+
   return {
     context,
     source,
-  
     data,
     update: () => {
       analyser.getByteFrequencyData(data);
@@ -69,3 +70,6 @@ async function createAudio() {
     },
   };
 }
+
+
+
